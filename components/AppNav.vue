b4a8accf765423c1eccf674fbe02d94ec0515676
Al. Schetinin <alexsander.qw@gmail.com>
2024-07-30 18:58:08 +0500

    Добавяле ссылку на фигму в навигацию

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ArrowUpRightIcon } from "lucide-vue-next";
 const links = [
   {
     id: 1,
@@ -15,6 +16,12 @@ const links = [
     name: "Статьи",
     link: "/blog",
   },
+  {
+    id: 4,
+    name: "Figma",
+    link: "https://www.figma.com/design/hbIk5zwFkjDPdjLsg5nG8n/%D0%A8%D0%B0%D0%B9%D0%B4%D1%83%D0%BB%D0%BB%D0%B8%D0%BD%D0%B0-%D0%A1%D0%B0%D1%84%D0%B8%D1%8F-%7C-Product-desigher?node-id=0-1",
+    external: true,
+  },
 ];
 </script>
 
@@ -22,7 +29,20 @@ const links = [
   <nav>
     <ul class="lg:space-y-3 max-lg:flex max-lg:justify-between max-lg:px-4 m-0">
       <li v-for="link in links" :key="link.id" class="list-none px-0 m-0">
-        <NuxtLink :to="link.link" class="opacity-70">{{ link.name }}</NuxtLink>
+        <NuxtLink
+          v-if="!link.external"
+          :to="link.link"
+          class="opacity-70 hover:opacity-100 transition-all"
+          >{{ link.name }}</NuxtLink
+        >
+        <a
+          v-else
+          :href="link.link"
+          target="_blank"
+          class="opacity-70 hover:opacity-100 transition-all flex gap-0.5 items-center"
+        >
+          {{ link.name }} <ArrowUpRightIcon class="w-4 h-4 mt-0.5"
+        /></a>
       </li>
     </ul>
   </nav>
