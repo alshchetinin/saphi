09fa71bb5b13b8c1480313b322a6be9487117554
Al. Schetinin <alexsander.qw@gmail.com>
2024-08-03 15:04:53 +0500

    fix

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -7,7 +7,7 @@
       <NuxtImg src="avatar.jpg" class="rounded-lg mb-1" />
       <div>
         <h2 c class="lg:mt-3 mb-0 text-lg">Шайдуллина Сафия</h2>
-        <p class="!mt-0 opacity-70 text-sm">Product desigher</p>
+        <p class="!mt-0 opacity-70 text-sm">Product designer</p>
       </div>
     </NuxtLink>
     <div class="lg:mt-20">
